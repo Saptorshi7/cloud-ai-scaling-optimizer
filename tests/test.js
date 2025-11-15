ba3c0fb033e64b59901de212fb69be513d1ae376
Saptorshi7 <saptorshi98@gmail.com>
2025-11-16 00:11:44 +0530

    add k6 test

---
+++ tests/test.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 20,                // 20 virtual users
+  duration: '30s',        // run for 30 seconds
+};
+
+export default function () {
+  http.get('http://demo-alb-1414112138.us-east-1.elb.amazonaws.com');
+  sleep(1);
+}
