4c8d1064d9585a8f3de77de9c9f7752dff64009b
Saptorshi7 <saptorshi98@gmail.com>
2025-11-16 17:42:23 +0530

    change scaling policy

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -2,7 +2,7 @@ output "lb_dns_name" {
   value = aws_lb.app.dns_name
 }
 output "target_group_arn" {
-  value = aws_lb_target_group.tg.arn
+  value = aws_lb_target_group.tg.arn_suffix
 }
 output "alb_sg_id" {
   value = aws_security_group.alb_sg.id
