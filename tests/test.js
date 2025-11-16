b4720a53acc2b11eaf425d6d6fcc4cff2d5ad422
Saptorshi7 <saptorshi98@gmail.com>
2025-11-16 11:38:17 +0530

    modify test

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 100,                // 20 virtual users
+  vus: 500,                // 20 virtual users
   duration: '300s',        // run for 30 seconds
 };
 
