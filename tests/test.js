2ed6996dbe6984ee4250848084e9f0c9761ca292
Saptorshi7 <saptorshi98@gmail.com>
2025-11-16 11:32:34 +0530

    modify test

--- tests/test.js
+++ tests/test.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 20,                // 20 virtual users
+  vus: 100,                // 20 virtual users
   duration: '300s',        // run for 30 seconds
 };
 
 export default function () {
-  http.get('http://demo-alb-1414112138.us-east-1.elb.amazonaws.com');
+  http.get('http://demo-alb-348654722.us-east-1.elb.amazonaws.com');
   sleep(1);
 }
